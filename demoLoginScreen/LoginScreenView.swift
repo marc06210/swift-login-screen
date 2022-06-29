1af5e215dc7155c294b2a10fbe4f87b56de10229
Marc Guerrini <maguerrini@airfrance.fr>
2022-06-29 15:07:46 +0200

    only ui

---
+++ demoLoginScreen/LoginScreenView.swift
@@ -0,0 +1,93 @@
+//
+//  LoginScreenView.swift
+//  demoLoginScreen
+//
+//  Created by Marc Guerrini on 29/06/2022.
+//
+
+import SwiftUI
+
+struct LoginScreenView: View {
+    @State var username = ""
+    @State var password = ""
+    @State var displayLoginSection = false
+    
+    var body: some View {
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            // here put your logo
+            VStack {
+                Image(systemName: "applelogo")
+                    .font(.system(size: 72))
+                    .padding(.top, displayLoginSection ? 20 : 0)
+                if displayLoginSection {
+                    Spacer()
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + DispatchTimeInterval.seconds(3)) {
+                    withAnimation {
+                        displayLoginSection = true
+                    }
+                }
+            }
+            
+            if displayLoginSection {
+                VStack {
+                    Spacer()
+                    TextField("", text: $username)
+                        .modifier(PlaceholderStyle(showPlaceHolder: username.isEmpty, placeholder: "username"))
+                        .padding(.horizontal, 5)
+                        .padding(.vertical, 2)
+                        .overlay(RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(Color.white.opacity(0.20), lineWidth: 2)
+                        )
+                    SecureField("", text: $password)
+                        .modifier(PlaceholderStyle(showPlaceHolder: password.isEmpty, placeholder: "password"))
+                        .padding(.horizontal, 5)
+                        .padding(.vertical, 2)
+                        .overlay(RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(Color.white.opacity(0.20), lineWidth: 2)
+                        )
+                    
+                    if username != "" && password != "" {
+                        Button {
+                            
+                        } label: {
+                            Text("Login")
+                        }
+                        .padding(.top)
+                    }
+                    Spacer()
+                }
+                .transition(.move(edge: .bottom))
+                .padding()
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct PlaceholderStyle: ViewModifier {
+    var showPlaceHolder: Bool
+    var placeholder: String
+
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if showPlaceHolder {
+                Text(placeholder)
+                    .padding(5.0)
+                    .foregroundColor(.white.opacity(0.50))
+            }
+            content
+            .padding(5.0)
+        }
+        .background(.blue)
+    }
+}
+
+struct LoginScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreenView()
+    }
+}
---
+++ demoLoginScreen/demoLoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoLoginScreenApp.swift
+//  demoLoginScreen
+//
+//  Created by Marc Guerrini on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct demoLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginScreenView()
+        }
+    }
+}
